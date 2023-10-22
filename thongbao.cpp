60ad1bb729d41f3ad9ae56b5cc906ee86ec60823
thanh nguyen <ntnthanhou@gmail.com>
2023-10-22 20:41:53 +0700

    them 1 dong tren branch feature-new day len github

--- thongbao.cpp
+++ thongbao.cpp
@@ -7,6 +7,7 @@ int main()
    int n; 
    cout<<"tao branch feature-new "<<endl;
    cout<<"them 1 thay doi vao branch feature-new o local"<<endl;
+   cout<<"add 1 dong o branch feature-new, day len github";
    return 0;
 }
 
