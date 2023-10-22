0bd7ea1292f7e2ca8ae94c2d60a80d4f412d2656
thanh nguyen <ntnthanhou@gmail.com>
2023-10-22 20:48:00 +0700

    main thay doi

--- thongbao.cpp
+++ thongbao.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main()
 {
    int n; 
-   cout<<"tao branch feature-new "<<endl;
+   cout<<"tao branch feature-new -----"<<endl;
    cout<<"them 1 thay doi vao branch feature-new o local"<<endl;
+   cout<<"add 1 dong o main, day len github-- main chinh dong nay";
    return 0;
 }
 
